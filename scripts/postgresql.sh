afe44f0c7a24f3fcf30cb84bcd2a06cb7b0ef5ee
ikame <anler86@gmail.com>
2014-05-07 15:50:20 +0200

    Adding missing dependencies to postgres provisioner

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -1,6 +1,14 @@
 # postgresql.sh
 
-apt-install-if-needed postgresql postgresql-contrib
+apt-install-if-needed postgresql-9.3 postgresql-contrib-9.3 \
+                      postgresql-doc-9.3 postgresql-server-dev-9.3 \
+                      postgresql-plpython3-9.3
 
 sudo -u postgres createuser --superuser $USER &> /dev/null
 sudo -u postgres createdb $USER &> /dev/null
+
+function createdb-if-needed {
+    for dbname in $@; do
+        $(psql -l | grep -q "$dbname") || createdb "$dbname"
+    done
+}
