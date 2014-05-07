fe93b0a8013f0fa57bf1dcabee71db1c896c7fb8
ikame <anler86@gmail.com>
2014-05-07 15:54:03 +0200

    Taiga repos provisioner script

--- scripts/taiga.sh
+++ scripts/taiga.sh
@@ -1,3 +1,24 @@
 # taiga.sh
 
+# setup taiga-front
+cd data/taiga-front
+
+npm-install-if-needed gulp bower
+
+npm install
+bower install
+
+cd
+
+# setup taiga-back
 mkvirtualenv-if-needed taiga
+createdb-if-needed taiga
+workon taiga
+
+cd data/taiga-back
+
+pip install -r requirements.txt
+
+sh ./install.sh
+
+cd
