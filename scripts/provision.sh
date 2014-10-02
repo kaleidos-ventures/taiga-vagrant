a0a7214988d84e555fbe426b09841293effe171b
Andrey Antukh <niwi@niwi.be>
2014-10-02 15:29:37 +0200

    Improved reprovisioning.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,10 +1,13 @@
 sudo apt-get -y update
 sudo apt-get -y install git bash
 
+
+rm -rf taiga-scripts
 git clone https://github.com/taigaio/taiga-scripts.git
 cd taiga-scripts
 
 mkdir -p ~/.setup
+
 cat > ~/.setup/data.sh <<EOF
 scheme="http"
 hostname="localhost:8000"
