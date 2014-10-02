c1bb5fc976f7e7352af4aa851507c8322673f376
Andrey Antukh <niwi@niwi.be>
2014-10-02 14:21:39 +0200

    Minor fixes.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,7 +3,6 @@ sudo apt-get -y install git bash
 
 git clone https://github.com/taigaio/taiga-scripts.git
 cd taiga-scripts
-git checkout improvements
 
 mkdir -p ~/.setup
 cat > ~/.setup/data.sh <<EOF
